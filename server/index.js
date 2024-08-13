9b4c21e8434c5d28d5346d5d2b6bd1313a3d6d2b
so-zero <zero_s0@naver.com>
2024-08-13 10:50:56 +0900

    server model, router

--- server/index.js
+++ server/index.js
@@ -1,12 +1,12 @@
 const express = require("express");
 require("dotenv").config();
 const connectDB = require("./config/connectDB");
+const authRoutes = require("./routes/authRoutes");
 
 const app = express();
+app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+app.use("/api/auth", authRoutes);
 
 connectDB().then(() => {
   app.listen(process.env.PORT || 8080, () => {
---
+++ server/models/userModel.js
@@ -0,0 +1,38 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    lastLogin: {
+      type: Date,
+      default: Date.now,
+    },
+    isVerified: {
+      type: Boolean,
+      default: false,
+    },
+    resetPasswordToken: String,
+    resetPasswordExpiresAt: Date,
+    verificationToken: String,
+    verificationTokenExpiresAt: Date,
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
