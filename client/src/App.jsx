1c861611bc426c0a7d9cb383eca468af8ef845c9
so-zero <zero_s0@naver.com>
2024-08-15 14:22:25 +0900

    protected route, redirect

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,70 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useEffect } from "react";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Home from "./pages/Home";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
 import EmailVerify from "./pages/EmailVerify";
 import { Toaster } from "react-hot-toast";
+import { useAuthStore } from "./store/authStore";
+
+const ProtectedRoute = ({ children }) => {
+  const { isAuthenticated, user } = useAuthStore();
+
+  if (!isAuthenticated) {
+    return <Navigate to="/login" replace />;
+  }
+
+  if (!user.isVerified) {
+    return <Navigate to="/verify-email" replace />;
+  }
+
+  return children;
+};
+
+const RedirectUser = ({ children }) => {
+  const { isAuthenticated, user } = useAuthStore();
+
+  if (isAuthenticated && user.isVerified) {
+    return <Navigate to="/" replace />;
+  }
+
+  return children;
+};
 
 function App() {
+  const { checkAuth, isCheckingAuth } = useAuthStore();
+
+  useEffect(() => {
+    checkAuth();
+  }, [checkAuth]);
+
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/login" element={<Login />} />
+        <Route
+          path="/"
+          element={
+            <ProtectedRoute>
+              <Home />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/register"
+          element={
+            <RedirectUser>
+              <Register />
+            </RedirectUser>
+          }
+        />
+        <Route
+          path="/login"
+          element={
+            <RedirectUser>
+              <Login />
+            </RedirectUser>
+          }
+        />
         <Route path="/verify-email" element={<EmailVerify />} />
       </Routes>
       <Toaster />
