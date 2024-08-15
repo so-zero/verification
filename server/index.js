f718bde4f66afae90044ada0a09ddb30184bc12e
so-zero <zero_s0@naver.com>
2024-08-15 12:48:28 +0900

    error handler, cors

--- server/index.js
+++ server/index.js
@@ -1,14 +1,19 @@
 const express = require("express");
+const cors = require("cors");
 require("dotenv").config();
 const cookieParser = require("cookie-parser");
 const connectDB = require("./config/connectDB");
 const authRoutes = require("./routes/authRoutes");
+const { notFound, errorHandler } = require("./middleware/errorMiddleware");
 
 const app = express();
+app.use(cors({ origin: process.env.FRONTEND_URL, credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
+app.use(notFound);
+app.use(errorHandler);
 
 connectDB().then(() => {
   app.listen(process.env.PORT || 8080, () => {
